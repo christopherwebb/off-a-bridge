430ec2ab699200deb3e738e215d0409ddb542434
Robin Allen <robin.allen@greenmangaming.com>
2013-02-08 20:15:25 +0000

    basic map

--- main.js
+++ main.js
@@ -4,7 +4,29 @@ var context;
 
 var tileSize = 32;
 
+var mapWidth = 20;
+var mapHeight = 15;
+
+var objects = [];
+var map = [];
+
+function mapIndex(pos) {
+  return pos.y * mapWidth + pos.x;
+}
+
+function setMap(pos, tile) {
+  map[mapIndex(pos)] = tile;
+}
+
 function main() {
+
+  for (var y=0; y<mapHeight; y++)
+  for (var x=0; x<mapWidth; x++) {
+    if (x == 0 || y == 0 || x+1==mapWidth || y+1==mapHeight) {
+      setMap({x:x, y:y}, 'wall');
+    }
+  }
+
   canvas = document.createElement('canvas');
   canvas.width = 640;
   canvas.height = 480;
@@ -12,22 +34,60 @@ function main() {
 
   context = canvas.getContext('2d');
 
-  setTimeout(process, 1000/60);
+  setInterval(process, 1000/60);
+
+  objects.push(makeObject({x: 45, y: 45}));
 }
 
 function process() {
+  objects.forEach(function(object) {
+    object.process();
+  });
   draw();
 }
 
 function draw() {
+  drawMap();
+  drawObjects();
+}
 
-  for (var y=0; y<20; y++)
-  for (var x=0; x<20; x++) {
+function makeObject(pos) {
+  var vel = {x: 0, y: 0};
+  var gravity = .1;
+  function process() {
+    vel.y += gravity;
+    pos.x += vel.x;
+    pos.y += vel.y;
+  }
+  function draw(context) {
+    context.fillStyle = '#000';
+    context.fillRect(pos.x, pos.y, tileSize, tileSize);
+    context.fillStyle = '#f00';
+    context.fillRect(pos.x+1, pos.y+1, tileSize-2, tileSize-2);
+  }
+  return {draw: draw, process: process};
+}
+
+function drawObjects() {
+  objects.forEach(function(object) {
+    object.draw(context);
+  });
+}
+
+function drawMap() {
+  for (var y=0; y<mapHeight; y++)
+  for (var x=0; x<mapWidth; x++) {
+
+    var cell = map[mapIndex({x:x, y:y})];
+
     var px = x * tileSize;
     var py = y * tileSize;
     context.fillStyle = '#000';
     context.fillRect(px, py, tileSize, tileSize);
-    context.fillStyle = '#ff0';
+    if (cell == 'wall')
+      context.fillStyle = '#a66';
+    else
+      context.fillStyle = '#eef';
     context.fillRect(px+1, py+1, tileSize-2, tileSize-2);
   }
 }
